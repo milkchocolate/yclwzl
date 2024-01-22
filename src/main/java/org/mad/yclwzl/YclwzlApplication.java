05c6cd6656c248e4023c7ac6ca4452fe9909b8ca
Nan Yu <nyu@Nans-MacBook-Pro.local>
2024-01-22 17:01:57 +0800

    Create the app

---
+++ src/main/java/org/mad/yclwzl/YclwzlApplication.java
@@ -0,0 +1,13 @@
+package org.mad.yclwzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YclwzlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YclwzlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mad/yclwzl/YclwzlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mad.yclwzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YclwzlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
